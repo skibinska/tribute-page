f0cdefcc4d64d67eb704890d42731c3ad0957e61
ewelina <skibinska.ewelina@gmail.com>
2017-04-08 21:32:22 +0100

    Add custom styles and Main.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import Main from './Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Main />
       </div>
     );
   }
-
 }
 
 export default App;
